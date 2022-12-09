bd4b5c9e93716231e151e81d9869c698a9810369
anis.benghanem <anis.benghanem@insat.ucar.tn>
2022-12-09 13:06:33 +0100

    tesdt

--- script.groovy
+++ script.groovy
@@ -18,9 +18,9 @@ def runUnitTests() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t hanaghz/demo-app:${IMAGE_NAME} .'
+        sh 'docker build -t anisbg/async-communication:${IMAGE_NAME} .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push hanaghz/demo-app:${IMAGE_NAME}'
+        sh 'docker push anisbg/async-communication:${IMAGE_NAME}'
     }
 }
 
