298e8eda9d320dab9744afd054507bd9705ec48b
anis.benghanem <anis.benghanem@insat.ucar.tn>
2022-12-09 11:35:19 +0100

    final

--- script.groovy
+++ script.groovy
@@ -26,7 +26,7 @@ def buildImage() {
 
 def sonarTest() {
     echo "Running sonarQube checks..."
-    sh 'mvn clean verify sonar:sonar   -Dsonar.projectKey=DevOpsProject   -Dsonar.host.url=http://localhost:9000   -Dsonar.login=sqp_c114049756b2a45c74b9a78aec56f2d48134dd4b'
+    sh 'mvn clean verify sonar:sonar   -Dsonar.projectKey=DevOpsProject   -Dsonar.host.url=http://172.21.0.3:9000   -Dsonar.login=sqp_c114049756b2a45c74b9a78aec56f2d48134dd4b'
 }
 
 
